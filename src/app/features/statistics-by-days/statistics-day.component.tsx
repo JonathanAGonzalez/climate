a2fb01f83108efedeb7bcee772ff7c93291aa100
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-06-26 13:03:27 -0300

    feat(statistics): add new component statistic by day

--- src/app/features/statistics-by-days/statistics-day.component.tsx
+++ src/app/features/statistics-by-days/statistics-day.component.tsx
@@ -1,9 +1,23 @@
-export const StatisticsDay = () => {
+import { HistoryData } from '@/app/core/types/history.type';
+import { formatTemperature } from '@/app/core/utils/fortmatTemperature';
+
+type StatisticsByDaysProps = {
+  statisticDay: HistoryData;
+};
+
+export const StatisticsDay = ({ statisticDay }: StatisticsByDaysProps) => {
+  const datetime = statisticDay.dt_txt;
+  const time = datetime.substring(11, 16);
+  const temperature = formatTemperature(statisticDay.main.temp);
+  const img = `https://openweathermap.org/img/wn/${statisticDay.weather[0].icon}@2x.png`;
+  const timeToShow = datetime.split(' ');
+
   return (
-    <article className='border flex flex-col justify-center items-center w-[79px] text-[#f3f3f3]'>
-      <h2>06h</h2>
-      <p>Clear Sky</p>
-      <p>11.7c</p>
+    <article className='flex flex-col justify-center items-center w-[79px] text-[#f3f3f3]'>
+      <h2>{time}h</h2>
+      <h3 className='text-xs'>{timeToShow[0]}</h3>
+      <img src={img} alt='Image ' />
+      <p>{temperature} c</p>
     </article>
   );
 };
