8994964bbf62506016ac41318669c0f7ef79e516
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-06-26 12:13:57 -0300

    refactor(home): add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,16 @@
-import { Divider } from './core/divider/divider.component';
+'use client';
+import { Toaster } from 'sonner';
 import { LayoutColumns } from './core/layout/layout-columns.component';
-import { ListItems } from './features/search/component/search-last-searched.component';
-import { Search } from './features/search/component/search.component';
-import { StatisticsByDays } from './features/statistics-by-days/statistics-by-days.component';
-import { StatisticsDay } from './features/statistics-by-days/statistics-day.component';
-const MOCK_LAST_SEARCHED = [
-  {
-    id: 1,
-    name: 'Buenos Aires',
-  },
-  {
-    id: 2,
-    name: 'Paris',
-  },
-  {
-    id: 3,
-    name: 'Barcelona',
-  },
-  {
-    id: 4,
-    name: 'Brasil',
-  },
-];
+import { WatherInformation } from './features/weather-information/weather-information.component';
+import { WeatherList } from './features/weather-information/weather-list.component';
 
 export default function Home() {
   return (
     <main>
+      <Toaster richColors position='top-center' />
       <LayoutColumns
-        contentFirstColumn={<div>1</div>}
-        contentSecondColumn={
-          <>
-            <Search />
-            <ListItems
-              list={MOCK_LAST_SEARCHED}
-              textGrayActive
-              rulesCss='h-[357px] pt-9'
-            />
-            <Divider />
-            <ListItems list={MOCK_LAST_SEARCHED} rulesCss='pt-4' />
-            <StatisticsByDays />
-          </>
-        }
+        contentFirstColumn={<WatherInformation />}
+        contentSecondColumn={<WeatherList />}
       />
     </main>
   );
