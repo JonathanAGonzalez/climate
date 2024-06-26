90cfea21b371a16605c777f0f89c922e797de745
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-06-26 13:03:01 -0300

    feat(statistics): add new component statistic by day

--- src/app/features/statistics-by-days/statistics-by-days.component.tsx
+++ src/app/features/statistics-by-days/statistics-by-days.component.tsx
@@ -1,11 +1,16 @@
 import { StatisticsDay } from './statistics-day.component';
+import { useWeatherStore } from '@/app/core/store/weather.store';
 
 export const StatisticsByDays = () => {
+  const statisticsDays = useWeatherStore((store) => store.historyData);
+
+  if (!statisticsDays) return;
+
   return (
-    <section className='flex gap-4 justify-center pt-12'>
-      <StatisticsDay />
-      <StatisticsDay />
-      <StatisticsDay />
+    <section className='flex gap-4 justify-center items-start pt-12'>
+      {statisticsDays.map((statistics, i) => (
+        <StatisticsDay key={i} statisticDay={statistics} />
+      ))}
     </section>
   );
 };
